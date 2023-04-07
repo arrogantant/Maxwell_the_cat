2a645cd5369c3bc995de213fa81dff434c303bd9
Junseong <david991129@naver.com>
2023-04-07 23:08:42 +0900

    Player Move

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{  
+    [SerializeField] float runSpeed = 10f;
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput =value.Get<Vector2>();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * runSpeed , myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+}
