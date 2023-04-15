15041e90bfbe137c2ae18855e5ae4e91b4925a4b
Junseong <david991129@naver.com>
2023-04-15 11:05:21 +0900

    Add acceleration&deceleration

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -9,14 +9,14 @@ public class Player : MonoBehaviour
     [SerializeField] float jumpForce = 5.0f;
     [SerializeField] LayerMask groundLayer;
     [SerializeField] float checkDistance = 0.1f;
+    [SerializeField] float acceleration = 15.0f;
+    [SerializeField] float deceleration = 40.0f;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private bool isGrounded = false;
     private Vector2 moveDirection;
-    private bool jumpRequest = false;
     Animator myAnimator;
     [SerializeField] Vector2 groundCheckOffset;
-    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -28,28 +28,40 @@ public class Player : MonoBehaviour
     {
         if (GetComponent<PlayerInput>().actions["Jump"].triggered && isGrounded)
         {
-            jumpRequest = true;
+            Jump();
         }
     }
 
     void FixedUpdate()
     {
-        Vector2 movement = new Vector2(moveDirection.x * speed, rb.velocity.y);
+        float targetSpeed = moveDirection.x * speed;
+        float currentSpeed = rb.velocity.x;
+
+        if (Mathf.Abs(moveDirection.x) > 0)
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, targetSpeed, acceleration * Time.fixedDeltaTime);
+        }
+        else
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, targetSpeed, deceleration * Time.fixedDeltaTime);
+        }
+
+        Vector2 movement = new Vector2(currentSpeed, rb.velocity.y);
         rb.velocity = movement;
-        
+
+ 
         Vector2 checkPosition = new Vector2(transform.position.x + groundCheckOffset.x, transform.position.y + groundCheckOffset.y);
         isGrounded = Physics2D.Raycast(checkPosition, Vector2.down, checkDistance, groundLayer);
-
-        if (jumpRequest)
-        {
-            Jump();
-            jumpRequest = false;
-        }
     }
 
     void Jump()
     {
-        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        Vector2 movement = new Vector2(moveDirection.x * speed, rb.velocity.y);
+        rb.velocity = movement;
+        if (GetComponent<PlayerInput>().actions["Jump"].triggered && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
     public void OnMove(InputValue value)
