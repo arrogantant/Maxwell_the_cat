5e377d16db006df26455ae73029cacfd93091d25
Junseong <david991129@naver.com>
2023-04-16 22:41:46 +0900

    Add Dash

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -12,6 +12,12 @@ public class Player : MonoBehaviour
     [SerializeField] float acceleration = 15.0f;
     [SerializeField] float deceleration = 40.0f;
     [SerializeField] float groundCheckRadius = 0.15f;
+    [SerializeField] float dashForce = 10.0f;
+    [SerializeField] float dashDuration = 0.1f;
+    [SerializeField] float dashCooldown = 0.5f;
+    private bool isDashing = false;
+    private bool canDash = true;
+    private int dashCount = 1;
     
     private Rigidbody2D rb;
     private SpriteRenderer sr;
@@ -41,10 +47,20 @@ public class Player : MonoBehaviour
         {
             myAnimator.SetBool("Jump", true);
         }
+
+        //대쉬
+    if (GetComponent<PlayerInput>().actions["Dash"].triggered && !isDashing && dashCount > 0 && canDash) // 'canDash' 변수 사용
+    {
+        StartCoroutine(Dash());
+        dashCount--; // 대쉬 횟수 감소
+        canDash = false; // 대쉬를 사용한 후 다시 사용할 수 없도록 함
+    }
     }
 
     void FixedUpdate()
     {
+        if (!isDashing)
+        {
         float targetSpeed = moveDirection.x * speed;
         float currentSpeed = rb.velocity.x;
         
@@ -63,6 +79,7 @@ public class Player : MonoBehaviour
  
         Vector2 checkPosition = new Vector2(transform.position.x + groundCheckOffset.x, transform.position.y + groundCheckOffset.y);
         isGrounded = Physics2D.OverlapCircle(checkPosition, groundCheckRadius, groundLayer);
+        }
     }
 
     void Jump()
@@ -73,6 +90,27 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+    }
+
+    IEnumerator Dash()
+    {
+        isDashing = true;
+        canDash = false; // 대쉬 중에는 대쉬를 사용할 수 없도록 함
+        float dashStartTime = Time.time;
+        Vector2 originalVelocity = rb.velocity;
+        Vector2 dashVelocity = new Vector2(dashForce * (sr.flipX ? -1 : 1), 0);
+
+        while (Time.time < dashStartTime + dashDuration)
+        {
+            rb.velocity = dashVelocity;
+            yield return null;
+        }
+
+        rb.velocity = originalVelocity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown); // 대쉬 쿨다운 대기
+        canDash = true; // 쿨다운이 끝나면 대쉬를 다시 사용할 수 있음
+        dashCount++; // 대쉬 쿨타임이 끝나면 대쉬 횟수를 증가시킴
     }
 
     public void OnMove(InputValue value)
