f5a2fee62480b0aadb8bcac9d3fbd2e208a9021d
Junseong <david991129@naver.com>
2023-04-26 19:28:38 +0900

    Add BackGruond

--- Assets/Free Pixel Art Hills/Demo/MoveBackground.cs
+++ Assets/Free Pixel Art Hills/Demo/MoveBackground.cs
@@ -2,22 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveBackground : MonoBehaviour 
-{
+public class MoveBackground : MonoBehaviour {
+
+
+
 	public float speed;
 	private float x;
 	public float PontoDeDestino;
 	public float PontoOriginal;
-		void Update () 
-		{
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		//PontoOriginal = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
 		x = transform.position.x;
 		x += speed * Time.deltaTime;
 		transform.position = new Vector3 (x, transform.position.y, transform.position.z);
 
-		if (x <= PontoDeDestino)
-		{
+
+
+		if (x <= PontoDeDestino){
+
+			
 			x = PontoOriginal;
 			transform.position = new Vector3 (x, transform.position.y, transform.position.z);
 		}
+
+
 	}
 }
