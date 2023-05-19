51840635bcea37001ddd35323a562fe2b2ab85c7
Junseong <david991129@naver.com>
2023-05-20 00:02:09 +0900

    Add pinball obj

---
+++ Assets/Script/pinball/pinballpad.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pinballpad : MonoBehaviour
+{
+    public float bounceForce = 10f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            Vector2 diagonalForce = new Vector2(1, 1).normalized * bounceForce; // 대각선 방향으로 힘을 적용
+            rb.AddForce(diagonalForce, ForceMode2D.Impulse);
+        }
+    }
+}
