4a53a837d4e80818d25ff4a0857292e8c67ed0eb
Junseong <david991129@naver.com>
2023-05-28 04:02:57 +0900

    Add map

--- Assets/Script/Obj/BouncePad.cs
+++ Assets/Script/Obj/BouncePad.cs
@@ -6,11 +6,11 @@ public class BouncePad : MonoBehaviour
 {
     public float bounceForce = 10f;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
-            Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+            Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
             rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
         }
