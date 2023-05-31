fa8f4a61a92cb147867fa0989acd34fb8fbfa20b
Junseong <david991129@naver.com>
2023-06-01 04:46:20 +0900

    Add Start screen

---
+++ Assets/Script/BackGroun/Animeition.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animeition : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+            animator.SetTrigger("run"); // 'Run' 애니메이션 상태를 실행
+    }
+}
