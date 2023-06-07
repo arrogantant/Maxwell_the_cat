ca93b66986f1989190254537852ae16e08727f09
Junseong <david991129@naver.com>
2023-06-08 02:38:56 +0900

    Add buttStomp

--- Assets/Script/Obj/ButtStomp.cs
+++ Assets/Script/Obj/ButtStomp.cs
@@ -4,21 +4,32 @@ using UnityEngine;
 
 public class ButtStomp : MonoBehaviour
 {   
-    public Animator animator;
     public BoxCollider2D solidCollider;
     public BoxCollider2D triggerCollider;
+    public GameObject DashObj;
+    public GameObject GoodObj;
+    public SpriteRenderer spriteRenderer;  // 스프라이트 렌더러 참조 추가
+    private Player playerScript; // Player 스크립트 참조
+
+    private void Start()
+    {
+        playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>(); // Player 스크립트 인스턴스를 가져옵니다.
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject == playerScript.gameObject && playerScript.isButtStomping)
+        {
+            Break();
+        }
+    }
 
     public void Break()
     {
         solidCollider.enabled = false; // 콜라이더 비활성화
         triggerCollider.enabled = true; // 트리거 콜라이더 활성화
-        animator.SetTrigger("ButtStomp"); // 파편 애니메이션 실행
-        StartCoroutine(DestroyAfterDelay(3f)); // 3초 후에 오브젝트 파괴
-    }
-
-    private IEnumerator DestroyAfterDelay(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        Destroy(gameObject);
+        DashObj.SetActive(false);
+        GoodObj.SetActive(true);
+        spriteRenderer.enabled = false; // 스프라이트 렌더러 비활성화
     }
 }
