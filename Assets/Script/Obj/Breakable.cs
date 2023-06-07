fb386c633a0a95d947a845a5e22e7b403ab1f52c
Junseong <david991129@naver.com>
2023-06-08 04:50:50 +0900

    Add Tutorial Map Description

--- Assets/Script/Obj/Breakable.cs
+++ Assets/Script/Obj/Breakable.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class Breakable : MonoBehaviour
 {
-    public Animator animator;
     public BoxCollider2D solidCollider;
     public BoxCollider2D triggerCollider;
     public GameObject DashObj;
     public GameObject GoodObj;
+    public SpriteRenderer SR;
 
     public void Break()
     {
         solidCollider.enabled = false; // 콜라이더 비활성화
         triggerCollider.enabled = true; // 트리거 콜라이더 활성화
-        animator.SetTrigger("Break"); // 파편 애니메이션 실행
+        SR.enabled =false;
         DashObj.SetActive(false);
         GoodObj.SetActive(true);
-        StartCoroutine(DestroyAfterDelay(3f)); // 3초 후에 오브젝트 파괴
+        StartCoroutine(DestroyAfterDelay(2f)); // 3초 후에 오브젝트 파괴
     }
 
     private IEnumerator DestroyAfterDelay(float delay)
     {
-        yield return new WaitForSeconds(2);
+        yield return new WaitForSeconds(2f);
         GoodObj.SetActive(false);
         yield return new WaitForSeconds(delay);
         Destroy(gameObject);
